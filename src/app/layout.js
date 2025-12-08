96d1d6965acc182fc07253aa7c31820b9b074ff8
bismay70 <samalbismay5@gmail.com>
2025-12-08 20:40:47 +0530

    register page

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <CommonLayout>{children}</CommonLayout>}
+        <CommonLayout>{children}</CommonLayout>
       </body>
     </html>
   );
--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,5 +1,40 @@
+import RegisterForm from "@/components/auth/RegisterForm";
+import Link from "next/link";
+
 export default function RegisterPage() {
     return (
-        
+        <div className="min-h-screen w-full flex flex-col md:flex-row bg-gradient-to-br from-gray-100 to-gray-200">
+            <div className="w-full md:w-1/2 flex items-center justify-center p-8 lg:p-12">
+                <div className="w-full max-w-md space-y-8 bg-white p-8 rounded-lg shadow-lg">
+                    <div className="space-y-2 text-center">
+                       <h1 className="text-3xlfont-bold tracking-tight text-gray-900">
+                         Register
+                       </h1>
+                       <p className="text-sm text-gray-500">
+                        Create ur account to get started
+                       </p>
+                       <RegisterForm/>
+                       <p className="text-center text-sm text-gray-600">
+                        Already have an account ?
+                        <Link className="font-semibold ml-2 text-red-700 hover:text-red-500" href="/login">Log In</Link>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            
+                <div className="hidden md:flex w-1/2 p-12 items-center justify-center relative">
+                <div className="max-w-lg space-y-6 text-white z-10">
+                    <h2 className="text-4xl text-black font-medium">
+                        Join Palabras, create a blog  & share your thoughts
+                    </h2>
+                    <p className="text-right text-lg text-black font-bold">-Bismay</p>
+                </div>
+                <div className="absolute inset-0 bg-black opacity-50">
+                    <img src="https://imgs.search.brave.com/aTA9ev41lStr_e_CWw6yiOl_GvevHuALyQ51GmQfSBw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAxNS8w/My8yMi8xNS8yNi9i/bG9nLTY4NDc0OF82/NDAuanBn"
+                    className="absolute inset-0 object-cover w-full h-full"
+                    />
+                </div>
+                </div>
+        </div>
     )
 }
